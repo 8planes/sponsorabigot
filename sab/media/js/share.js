d431f30835e62357c7ec8bd8d77c1e0e18e57623
Adam Duston <aduston@gmail.com>
2011-04-08 18:26:28 -0400

    Added share buttons to landing page

--- sab/media/js/share.js
+++ sab/media/js/share.js
@@ -5,34 +5,47 @@ function formatAmount(dollarAmount) {
     return '$' + Math.floor(dollarAmount) + "." + cents;
 }
 
-function getMessage(dollarAmount) {
+function getFBDonateMessage() {
     return "I am donating " + formatAmount(dollarAmount) + " to " +
         "the Elizabeth Taylor AIDS Foundation for every hour " +
         "Westboro spends protesting.";
 }
 
-function facebookURL(dollarAmount) {
-    // see http://developers.facebook.com/docs/reference/dialogs/feed/
+function getFBSiteMessage() {
+    return "We have received pledges to donate " + 
+        formatAmount(TOTAL_AMOUNT) + " for every hour Westboro spends " +
+        "protesting. The goal is " + formatAmount(TOTAL_AMOUNT_GOAL) + 
+        ". Help out!";
+}
+
+function facebookURL(message) {
     var description = "Help us turn Westboro's protest of Liz Taylor's public memorial into a fundraising bonanza for the Elizabeth Taylor AIDS Foundation.";
     return 'http://www.facebook.com/dialog/feed' +
         '?app_id=' + FACEBOOK_APP_ID + 
         '&link=' + encodeURIComponent('http://sponsorabigot.org') +
         '&picture=' + encodeURIComponent(FB_IMAGE) +
         '&display=popup' +
-        '&message=' + encodeURIComponent(getMessage(dollarAmount)) +
+        '&message=' + encodeURIComponent(message) +
         '&description=' + encodeURIComponent(description) +
         '&redirect_uri=' + encodeURIComponent(POPUP_REDIRECT_URL);
 }
 
-function openFacebook() {
+function openFacebook(message) {
     window.open(
-        facebookURL(dollarAmount),
+        facebookURL(message),
         'post_to_fb', 'status=0,width=580,height=400');
 }
 
-function openTwitter() {
-    var msg = "Donating " + formatAmount(dollarAmount) + 
+function getTWDonateMessage() {
+    return "Donating " + formatAmount(dollarAmount) + 
         " to Elizabeth Taylor AIDS Foundation for every hour Westboro protests Liz's funeral #sponsorabigot";
+}
+
+function getTWSiteMessage() {
+    return "Donate to Elizabeth Taylor AIDS Foundation for every hour Westboro protests Liz's funeral. #sponsorabigot #greatidea";
+}
+
+function openTwitter(msg) {
     var url = "http://twitter.com/share?text=" + encodeURIComponent(msg) +
         "&url=" + encodeURIComponent('http://sponsorabigot.org');
     window.open(
@@ -41,12 +54,20 @@ function openTwitter() {
 }
 
 $(function() {
-    $('a.fb').click(function(e) {
+    $('a.fb-sharedonation').click(function(e) {
         e.preventDefault();
-        openFacebook();
+        openFacebook(getFBDonateMessage());
     });
-    $('a.twitter').click(function(e) {
+    $('a.fb-sharesite').click(function(e) {
         e.preventDefault();
-        openTwitter();
+        openFacebook(getFBSiteMessage());
+    });    
+    $('a.tw-sharedonation').click(function(e) {
+        e.preventDefault();
+        openTwitter(getTWDonateMessage());
+    });
+    $('a.tw-sharesite').click(function(e) {
+        e.preventDefault();
+        openTwitter(getTWSiteMessage());
     });
 });
